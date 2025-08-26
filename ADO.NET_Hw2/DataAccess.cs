0d74b515dd2f816e3c79933c3b78eb2c4bba407b
Oruco_vs53 <Oruco_vs53@STHQ0118-02>
2025-08-26 21:45:33 +0400

    Add project files.

---
+++ ADO.NET_Hw2/DataAccess.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NET_Hw2;
+
+class DataAccess
+{
+    DbConnection? conn = new SqlConnection("Data Source=STHQ0118-02;Initial Catalog=Library;User ID=admin;Password=admin;Connect Timeout=30;");
+
+    public void ShowBooks()
+    {
+        SqlDataReader? reader = null;
+        try
+        {
+            conn?.Open();
+
+            using SqlCommand cmd = new SqlCommand("SELECT Name FROM Books", (SqlConnection)conn);
+            reader = cmd.ExecuteReader();
+
+            int count = 0;
+            while (reader.Read())
+            {
+                count++;
+                Console.WriteLine($"{count}.{reader[0]}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+        finally
+        {
+            conn?.Close();
+            reader?.Close();
+        }
+    }
+
+
+
+}
---
+++ ADO.NET_Hw2/Program.cs
@@ -0,0 +1,12 @@
+﻿using ADO.NET_Hw2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var app = new DataAccess();
+        app.ShowBooks();
+
+
+    }
+}
