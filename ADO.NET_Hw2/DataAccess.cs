ca9990f7edc72f9450408f701c75604d72aa8d10
Burhan <burhanorucov@gmail.com>
2025-08-28 15:59:09 +0400

    last version

--- ADO.NET_Hw2/DataAccess.cs
+++ ADO.NET_Hw2/DataAccess.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
 using System.Linq;
@@ -10,36 +11,120 @@ namespace ADO.NET_Hw2;
 
 class DataAccess
 {
-    DbConnection? conn = new SqlConnection("Data Source=STHQ0118-02;Initial Catalog=Library;User ID=admin;Password=admin;Connect Timeout=30;");
+    DbConnection? conn = new SqlConnection("Server=localhost,1433;Database=library;User Id=sa;Password=MyS3cure_P@ssw0rd123;TrustServerCertificate=True;");
+    DbDataAdapter? adapter = null;
+    public DataTable? table = null;
+    public List<string> bookList = new List<string>();
+    public List<string> columnList = new List<string>();
 
-    public void ShowBooks()
+    private void ShowDataTable(DataTable table)
     {
-        SqlDataReader? reader = null;
-        try
-        {
-            conn?.Open();
+        foreach (DataColumn Dcolumn in table.Columns)
+            Console.Write($"{Dcolumn.ColumnName,-15}");
 
-            using SqlCommand cmd = new SqlCommand("SELECT Name FROM Books", (SqlConnection)conn);
-            reader = cmd.ExecuteReader();
+        Console.WriteLine();
 
-            int count = 0;
-            while (reader.Read())
-            {
-                count++;
-                Console.WriteLine($"{count}.{reader[0]}");
-            }
-        }
-        catch (Exception ex)
+        foreach (DataRow Drow in table.Rows)
         {
-            Console.WriteLine(ex.ToString());
+            bookList.Add(Drow["Column1"].ToString());
+            
+            foreach (var item in Drow.ItemArray)
+                Console.Write($"{item,-15}");
+            Console.WriteLine();
         }
-        finally
-        {
-            conn?.Close();
-            reader?.Close();
-        }
+
     }
 
+    public void FillTableWithDisconnect()
+    {
+        var query = "SELECT CAST(Id AS NVARCHAR) + '.' + Name FROM Books;";
 
+        var command = new SqlCommand()
+        {
+            CommandText = query,
+            Connection = (SqlConnection)conn
+        };
+
+        adapter = new SqlDataAdapter(command);
+        table = new DataTable();
+        adapter.Fill(table);
+        
+
+        ShowDataTable(table);
+    }
+    public void ShowColumns()
+    {
+        var query = "SELECT * FROM Books";
+
+        var command = new SqlCommand()
+        {
+            CommandText = query,
+            Connection = (SqlConnection)conn
+        };
+
+        adapter = new SqlDataAdapter(command);
+        DataTable table = new DataTable();
+        adapter.Fill(table); 
+
+        Console.WriteLine("Column Names:");
+        foreach (DataColumn column in table.Columns)
+        {
+            columnList.Add(column.ColumnName);
+            if (!column.ColumnName.Contains("Id"))//foregin keylerin hamisinda id oldugu ucun
+                Console.WriteLine(column.ColumnName);
+        }
+    }
+    public void UpdateBook(string bookName,string columnName,string newValue)
+    {
+        var query = $"UPDATE Books SET {columnName} = @newValue WHERE Name = @bookName;";
+
+        var command = new SqlCommand()
+        {
+            CommandText = query,
+            Connection = (SqlConnection)conn
+        };
+        command.Parameters.AddWithValue("@newValue", newValue);
+        command.Parameters.AddWithValue("@bookName", bookName);
+        adapter = new SqlDataAdapter(command);
+        conn.Open();
+        command.ExecuteNonQuery();
+        conn.Close();
+        Console.WriteLine("Updated");
+        
+    }
+    public void AddCascade()
+    {
+        var query = $"EXEC AddCascadeToSCards";
+        var command = new SqlCommand()
+        {
+            CommandText = query,
+            Connection = (SqlConnection)conn
+        };
+        adapter = new SqlDataAdapter(command);
+        conn.Open();
+        command.ExecuteNonQuery();
+        conn.Close();
+        Console.WriteLine("Added");
+    }
+
+    public void DeleteBook(string bookName)
+    {
+        var query2 = $"DELETE FROM Books WHERE Name = @bookName;";
+
+       
+        var command2 = new SqlCommand()
+        {
+            CommandText = query2,
+            Connection = (SqlConnection)conn
+        };
+        command2.Parameters.AddWithValue("@bookName", bookName);
+
+        conn.Open();
+        adapter = new SqlDataAdapter(command2);
+        command2.ExecuteNonQuery();
+        conn.Close();
+        Console.WriteLine("Deleted");
+        
+    }
 
 }
--- ADO.NET_Hw2/Program.cs
+++ ADO.NET_Hw2/Program.cs
@@ -1,12 +1,49 @@
-﻿using ADO.NET_Hw2;
+﻿using System.Text;
+using ADO.NET_Hw2;
 
 class Program
 {
     static void Main(string[] args)
     {
         var app = new DataAccess();
-        app.ShowBooks();
+        // app.AddCascade();//book-a reference eden foreign key-leri on cascade edirki delete edende problem yaranmasin.
+        // app.DeleteBook("SQL");
 
-
+        while (true)
+        {
+            app.FillTableWithDisconnect();
+            Console.WriteLine("Choose one");
+            string choice = Console.ReadLine();
+            Console.Clear();
+            try
+            {
+                Console.WriteLine(app.bookList[int.Parse(choice)-2]);
+            }
+            catch (Exception e)
+            {
+                Console.Clear();
+                Console.WriteLine("Wrong input.Please enter the numbers that are written");
+                Thread.Sleep(2000);
+                continue;
+            }
+            Console.WriteLine("1.Update");
+            Console.WriteLine("2.Delete");
+            string choice2 = Console.ReadLine();
+            if (choice2 == "1")
+            {
+                app.ShowColumns();
+                Console.Write("Choose one: ");
+                string choice3 = Console.ReadLine();
+                if (app.columnList.Contains(choice3))
+                {
+                    Console.Write("Enter new value: ");
+                    string newValue = Console.ReadLine();
+                    app.UpdateBook(app.bookList[int.Parse(choice)-2].Substring(2),choice3,newValue);
+                    Console.WriteLine("Successfully updated!");
+                    Thread.Sleep(2000);
+                }
+            }
+            
+        }
     }
 }
